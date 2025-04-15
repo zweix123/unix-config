c3714a162739a6db70dd8fe41aff340099f3b93c
zweix123 <1979803044@qq.com>
2025-04-15 11:51:37 +0800

    [fix] 修改json的cover逻辑, 添加关于golang的更多的配置

--- IDE/control.py
+++ IDE/control.py
@@ -1,12 +1,27 @@
 #! /usr/bin/env python3
 
+"""
+考虑一下相关行为:
+1. json标准格式不支持注释, 在vsc的配置文件中不支持注释, 但是在vsc的json文件可以添加注释, 作为配置文件, 添加注释也是好的, 所以在读取本地配置时, 过滤由//开头的单行或者行尾注释.
+2. 在json文件或者vsc的配置文件中, 都是允许重复的键的, 前者会飘黄, 后者会转灰, 其行为均是后出现的覆盖先出现的, json被python读取后变成dict, 键是唯一的, 其行为也是后者覆盖前者.
+3. 我们需要将这里的配置install到vsc的配置文件中, 或者对vsc的配置文件collect到这里的配置来, 其行为如下:
+   将增量的配置定义为a, 将被覆盖的配置定义为b
+   1. 假如一个键a有b没有: 直接将a的该键的值覆盖到b中
+   2. 假如一个键a有b也有:
+      1. dict类型: 递归
+      2. list类型: 合并去重
+      3. 原子类型: a的值覆盖b的值
+   3. 假如一个键a没有b有: 不做处理
+   上面有一个值得注意的点, 就是这意味着我们的配置对实际的配置是只能增不能删除的
+4. python标准库json的读取中, 要求map或者list的最后, 不能有行尾逗号, 但这些逗号在vsc的配置或者vsc的json中都是允许的, 需要手动删除
+"""
 
 import json
 import os
 import shutil
 import sys
 
-# from rich import print
+from rich import print
 
 # 区分Windows、Linux和macOS
 IS_WINDOWS = os.name == "nt"
@@ -27,12 +42,6 @@ CURSOR_SETTINGS = os.path.join(CURSOR_FOLDER, "User", "settings.json")
 SETTINGS = "./settings.json"
 
 
-def setting_corver(corver: dict, be_corvered: dict) -> dict:
-    for k, v in corver.items():
-        be_corvered[k] = v
-    return be_corvered
-
-
 def read_settings(settings_file: str) -> dict:
     content = ""
     # 手动过滤注释(并不是Json标准的东西)
@@ -49,6 +58,28 @@ def read_settings(settings_file: str) -> dict:
         print(settings_file)
         print(content)
         raise e
+
+
+def cover_settings(corver: dict, be_corvered: dict) -> dict:  # corver -> be_corvered
+    be_corvered_copy = be_corvered.copy()
+    for k, v in corver.items():
+        if k not in be_corvered_copy:
+            be_corvered_copy[k] = v
+        else:
+            if isinstance(v, dict):
+                be_corvered_copy[k] = cover_settings(v, be_corvered_copy[k])
+            elif isinstance(v, list):
+                # 需要保证列表元素都是原子元素
+                assert all(
+                    isinstance(i, int) or isinstance(i, str) or isinstance(i, bool)
+                    for i in v
+                )
+                # 去重合并
+                be_corvered_copy[k] = list(dict.fromkeys(v + be_corvered_copy[k]))
+            else:
+                assert isinstance(v, int) or isinstance(v, str) or isinstance(v, bool)
+                be_corvered_copy[k] = v
+    return be_corvered_copy
 
 
 def read_config_settings() -> dict:
@@ -84,11 +115,11 @@ def install():
     print("install IDE")
     settings = read_config_settings()
     if os.path.exists(VSCODE_SETTINGS):
-        write_vscode_settings(setting_corver(settings, read_vscode_settings()))
+        write_vscode_settings(cover_settings(settings, read_vscode_settings()))
     else:
         print("vscode settings not found")
     if os.path.exists(CURSOR_SETTINGS):
-        write_cursor_settings(setting_corver(settings, read_cursor_settings()))
+        write_cursor_settings(cover_settings(settings, read_cursor_settings()))
     else:
         print("cursor settings not found")
 
@@ -114,7 +145,7 @@ def collect():
     """
     if os.path.exists(CURSOR_SETTINGS):
         write_config_settings(
-            setting_corver(read_cursor_settings(), read_config_settings())
+            cover_settings(read_cursor_settings(), read_config_settings())
         )
     else:
         print("cursor settings not found")
